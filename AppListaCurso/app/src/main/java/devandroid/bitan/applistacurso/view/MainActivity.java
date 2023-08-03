c1195225be14b5f4782b85f1f040f2679cabef51
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-08-03 13:07:31 -0300

    SharedPreferences Limpar dados salvos.

--- AppListaCurso/app/src/main/java/devandroid/bitan/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/bitan/applistacurso/view/MainActivity.java
@@ -17,6 +17,7 @@ import devandroid.bitan.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
     public static final String NOME_PREFERENCES = "pref_listavip";
     PessoaController controller;
     Pessoa pessoa;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES,0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
 
         controller = new PessoaController();
         controller.toString();
@@ -67,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
                 editSobreNomeAluno.setText("");
                 editTelefoneContato.setText("");
                 editNomeCurso.setText("");
+
+                listaVip.clear();
+                listaVip.apply();
             }
         });
         btnFinalizar.setOnClickListener(new View.OnClickListener() {
