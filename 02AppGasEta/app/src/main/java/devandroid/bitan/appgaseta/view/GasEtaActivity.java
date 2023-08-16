64bdca076cfba7993a71d93a5c2088b82e652f21
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-08-16 14:11:38 -0300

    Implementação App GasEta - Gasolina X Etanol

---
+++ 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/view/GasEtaActivity.java
@@ -0,0 +1,19 @@
+package devandroid.bitan.appgaseta.view;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.bitan.appgaseta.R;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+    }
+}
