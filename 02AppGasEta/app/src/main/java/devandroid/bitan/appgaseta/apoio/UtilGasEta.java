c26a0d3d991f0ca5f5ecf5733bfde36b95fa54c4
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-08-18 14:49:34 -0300

    Declaração dos campos editText Gasolina/Etano/Resultado, botões Calcular/Salvar/Limpar/Finalizar e implementação de ação no botão Finalizar.

--- 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/apoio/UtilGasEta.java
+++ 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/apoio/UtilGasEta.java
@@ -10,4 +10,26 @@ public class UtilGasEta {
         return "Qualquer mensagem...";
     }
 
+    public static double preco(){
+        return 9.99;
+    }
+
+    public static String calcularMelhorOpcao(double gasolina, double etanol){
+        // preco da gasolina: R$ 5,12
+        // preco do etanol: R$ 3,99
+
+        // preco ideal  = gasolina * 0,70 = R$ 3,548
+
+        double precoIdeal = gasolina * 0.70;
+        String mensagemDeRetorno;
+
+        if(etanol<=precoIdeal){
+            mensagemDeRetorno = "Abastecer com Etanol";
+        }else {
+            mensagemDeRetorno = "Abastecer com Gasolina";
+        }
+
+        return mensagemDeRetorno;
+    }
+
 }
--- 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/view/GasEtaActivity.java
+++ 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/view/GasEtaActivity.java
@@ -2,6 +2,10 @@ package devandroid.bitan.appgaseta.view;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -12,7 +16,15 @@ import devandroid.bitan.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
 
-    //UtilGasEta utilGasEta = new UtilGasEta();
+    EditText editGasolina;
+    EditText editEtanol;
+
+    TextView txtResultado;
+
+    Button btnCalcular;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,11 +32,46 @@ public class GasEtaActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_gaseta);
 
-        //utilGasEta.metodoEstatico();
+        editGasolina = findViewById(R.id.editGasolina);
+        editEtanol = findViewById(R.id.editEtanol);
 
-        //UtilGasEta.metodoNaoEstatico();
+        txtResultado = findViewById(R.id.txtResultado);
 
-        Toast.makeText(GasEtaActivity.this, UtilGasEta.mensagem(),
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GasEtaActivity.this, "GasEta - Boa Economia!!!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        Toast.makeText(GasEtaActivity.this, UtilGasEta.calcularMelhorOpcao(5.12, 3.59),
                 Toast.LENGTH_LONG).show();
     }
 }
