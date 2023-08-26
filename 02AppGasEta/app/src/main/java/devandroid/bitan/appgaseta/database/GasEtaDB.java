d303534f87b9f5a6e8c8bdcb63318cd79a96fa5c
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-08-26 09:17:28 -0300

    Criação de banco de dados, tabela Veiculo e tabela Combustivel.

---
+++ 02AppGasEta/app/src/main/java/devandroid/bitan/appgaseta/database/GasEtaDB.java
@@ -0,0 +1,46 @@
+package devandroid.bitan.appgaseta.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class GasEtaDB extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "gaseta.db";
+    public static final int DB_VERSION = 1;
+
+    Cursor cursor;
+    SQLiteDatabase db;
+
+    public GasEtaDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sqlTabelaVeiculo = "CREATE TABLE Veiculo (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nomeDoCarro TEXT, " +
+                "placaDoCarro TEXT)";
+
+        String sqlTabelaCombustivel = "CREATE TABLE Combustivel (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nomeDoCombustivel TEXT, " +
+                "precoDoCombustivel REAL, " +
+                "recomendacao TEXT)";
+
+        sqLiteDatabase.execSQL(sqlTabelaVeiculo);
+        sqLiteDatabase.execSQL(sqlTabelaCombustivel);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+
+    // Criar métodos para implementar CRUD
+    // Nome do BD
+    // Versão do BD
+}
